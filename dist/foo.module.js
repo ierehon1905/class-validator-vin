3c495290f9a3aac46a0859d34c9f3150362142ea
Leon Minasyan <ierehon1905@gmail.com>
2021-06-08 17:41:51 +0300

    lib update

--- dist/foo.module.js
+++ dist/foo.module.js
@@ -1,2 +1,2 @@
-import{registerDecorator as t}from"class-validator";function n(n){return function(r,a){t({name:"isVIN",target:r.constructor,propertyName:a,constraints:[],options:n,validator:{validate:function(t,n){return/[0123456789ABCDEFGHJKLMNPRSTUVWXYZ]{17}/i.test(t)},defaultMessage:function(t){return"String ($value) is not a valid VIN!"}}})}}export{n as IsVIN};
+import{registerDecorator as t}from"class-validator";function n(n){return function(r,a){t({name:"isVIN",target:r.constructor,propertyName:a,constraints:[],options:n,validator:{validate:function(t,n){return/^[0123456789ABCDEFGHJKLMNPRSTUVWXYZ]{17}$/i.test(t)},defaultMessage:function(t){return"String ($value) is not a valid VIN!"}}})}}export{n as IsVIN};
 //# sourceMappingURL=foo.module.js.map
