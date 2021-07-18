5616ce0597f51c41c505818092ea5e05fb5431e0
Leon Minasyan <ierehon1905@gmail.com>
2021-07-18 18:38:50 +0300

    update

--- dist/foo.cjs
+++
@@ -1,2 +0,0 @@
-var t=require("class-validator");exports.IsVIN=function(r){return function(e,a){t.registerDecorator({name:"isVIN",target:e.constructor,propertyName:a,constraints:[],options:r,validator:{validate:function(t,r){return/^[0123456789ABCDEFGHJKLMNPRSTUVWXYZ]{17}$/i.test(t)},defaultMessage:function(t){return"String ($value) is not a valid VIN!"}}})}};
-//# sourceMappingURL=foo.cjs.map
